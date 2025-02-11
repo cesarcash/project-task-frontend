23282238afdc2bd679c5ff69876c66b53f4ed9f7
cesar salazar <cesarcash5@gmail.com>
2025-02-10 23:40:10 -0600

    creacion de login y register

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,31 @@
-import './Login.css';
+import '../blocks/PageLogin.css';
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
 
 const Login = () => {
 
     return (
-        <h1>Login</h1>
+
+        <div className="login">
+            <h1 className="login__title">App Task</h1>
+            <form className="form form--login">
+                <p className="login__paragraph">Inicio de sesión</p>
+                <div className="form__row">
+                    <input type="email" className="form__input" placeholder="Email" required />
+                </div>
+                <div className="form__row">
+                    <input type="password" className="form__input" placeholder="Password" required />
+                </div>
+                <div className="form__row">
+                    <button className='form__button' type="submit"><FontAwesomeIcon icon={faRightToBracket} /> Ingresar</button>
+                </div>
+                <p className="login__paragraph login__paragraph--small">
+                    ¿Aún no eres miembro? Regístrate <NavLink to="/register" className="login__paragraph_link">aquí</NavLink>
+                </p>
+            </form>                
+        </div>
+        
     );
 
 }
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,11 +1,34 @@
-import './Register.css';
+import '../blocks/PageLogin.css';
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
 
-const Register = () => {
+const Login = () => {
 
     return (
-        <h1>Registro</h1>
+
+        <div className="login">
+            <h1 className="login__title">App Task</h1>
+            <form className="form form--login">
+                <p className="login__paragraph">Registro</p>
+                <div className="form__row">
+                    <input type="text" className="form__input" placeholder="Nombre de usuario" required />
+                </div>
+                <div className="form__row">
+                    <input type="email" className="form__input" placeholder="Email" required />
+                </div>
+                <div className="form__row">
+                    <input type="password" className="form__input" placeholder="Password" required />
+                </div>
+                <div className="form__row">
+                    <button className='form__button' type="submit"><FontAwesomeIcon icon={faRightToBracket} /> Registrar</button>
+                </div>
+                <p className="login__paragraph login__paragraph--small">¿Ya eres miembro? Inicia sesión <NavLink to="/login" className="login__paragraph_link">aquí</NavLink></p>
+            </form>                
+        </div>
+        
     );
 
 }
 
-export default Register;
+export default Login;
