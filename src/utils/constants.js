232d3d94655143971fd066877f272e18b93addae
cesar salazar <cesarcash5@gmail.com>
2025-03-03 00:03:23 -0600

    actualizacion de url api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
 export const API_URL = 'http://api.quotable.io/';
-export const API_BACKEND = 'http://localhost:3000';
+// export const API_BACKEND = 'http://localhost:3000';
+export const API_BACKEND = 'https://api.apptask.twilightparadox.com';
